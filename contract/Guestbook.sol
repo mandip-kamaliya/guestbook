13b081f7125ab5f6dabd5d0d1c2101516fe08142
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-07-25 22:55:38 +0530

    add require check for given ether

--- contract/Guestbook.sol
+++ contract/Guestbook.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.8.20;
 
 contract Guestbook{
+
+    //maping
+    mapping (address=> string) public guestBook;
     
+
+    function post() public payable {
+        require(msg.value == 0.0001 ether, "You must pay 0.0001 ether to post.");
+
+    }   
 }
