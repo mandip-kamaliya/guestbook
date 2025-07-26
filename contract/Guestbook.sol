deedeb8df767c99b0b457d7831d24ddf0a3c46ea
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-07-26 08:39:33 +0530

    added Sign function in our contract

--- contract/Guestbook.sol
+++ contract/Guestbook.sol
@@ -3,16 +3,29 @@ pragma solidity ^0.8.20;
 
 contract Guestbook{
 
+    //constants
+    uint256  public constant SIGN_FEE=0.0001 ether;
+
+    //events
+    event SIGNTUREPOSTED(address indexed owner,string  message,uint256 timestamp);
+
     //struct
     struct signature{
         address owner;
         string message;
         uint256 timestamp;     
     }
+    signature[] public signatures;
     
 
-    function post() public payable {
-        require(msg.value == 0.0001 ether, "You must pay 0.0001 ether to post.");
+    function Sign(string memory _message) public payable {
+        require(msg.value == SIGN_FEE , "You must pay 0.0001 ether to post.");
+        signatures.push(signature({
+            owner:msg.sender,
+            message:_message,
+            timestamp:block.timestamp
+        }));
+        emit SIGNTUREPOSTED(msg.sender,_message,block.timestamp);
 
     }   
 }
